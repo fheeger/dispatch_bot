11135982f03ff05da17cc6bc32f1312be3bc96bb
Felix Heeger <fheeger@poste.de>
2022-03-23 19:25:31 +0100

    restrucutred bot script

--- bot_script.py
+++ bot_script.py
@@ -18,8 +18,14 @@ GET_MESSAGES_PATH = "get_messages/"
 CHECK_MESSAGES_PATH = "check_messages/"
 POST_MESSAGE_PATH = "send_message/"
 
-description = '''Dispatch Bot for IKS'''
-bot = commands.Bot(command_prefix='!', description=description)
+
+help_command = commands.DefaultHelpCommand(
+    no_category='Commands'
+)
+description = "Dispatch Bot for IKS"
+bot = commands.Bot(command_prefix='!',
+                   description=description,
+                   help_command = help_command)
 turn = 1
 
 headers = {
@@ -77,119 +83,97 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+class Misc(commands.Cog):
+    """Miscellaneous Commands"""
 
-@bot.command()
-async def help(ctx):
-    """print help message"""
-    help ="""
-     I am the Dispatch Bot and support the following commands:
-     !help                 -> Show this helpful message.
-     !hellp                -> Say hi.
-     !dispatch             -> Send everything that is in the message as a dispatch.
-     !start_game <name>    -> Start a new game called <name>. This will collect the currently available 
-                              BLUE and RED channels.
-     !next_turn            -> Advance the current game to the next turn and deliver all approved dispatches.
-    """
-    await ctx.send(help)
-
-@bot.command()
-async def hello(ctx):
-    """Says hello"""
-    await ctx.send("Hello I am the Dispatch Bot")
+    @bot.command()
+    async def hello(ctx):
+        """Says hello"""
+        await ctx.send("Hello I am the Dispatch Bot")
 
 
-@bot.command()
-async def dispatch(ctx, message: str):
-    """receive a dispatch from a player"""
-    try:
-        data = {
-            "text": ctx.message.content.split("!dispatch", 1)[1],
-            "sender": ctx.message.author.name
-        }
-        post_url(POST_MESSAGE_PATH, data)
-        await ctx.send("Dispatch was send")
-    except Exception as e:
-        await ctx.send("There was an error sending your dispatch:%s" % str(e)[:1000])
+class PlayerCommands(commands.Cog):
+    """Player Commands"""
+
+    @bot.command()
+    async def dispatch(ctx, message: str):
+        """receive a dispatch from a player"""
+        try:
+            data = {
+                "text": ctx.message.content.split("!dispatch", 1)[1],
+                "sender": ctx.message.author.name
+            }
+            post_url(POST_MESSAGE_PATH, data)
+            await ctx.send("Dispatch was send")
+        except Exception as e:
+            await ctx.send("There was an error sending your dispatch:%s" % str(e)[:1000])
 
 
-@bot.command()
-async def get_round(ctx):
-    res = get_url("get_round")
-    await ctx.send("This is round " + str(res['turn']))
+class UmpireCommands(commands.Cog):
+    """Umpire Commands"""
 
+    @bot.command()
+    async def start_game(ctx, name):
+       """start a new game"""
+       blue = {}
+       red = {}
+       for entry in ctx.guild.channels:
+           if entry.type == ChannelType.category:
+               if entry.name == RED_CATEGORY:
+                   for channel in entry.text_channels:
+                       red[channel.name] = channel.id
+               if entry.name == BLUE_CATEGORY:
+                   for channel in entry.text_channels:
+                       blue[channel.name] = channel.id
 
-@bot.command()
-async def next_turn(ctx):
-    """Tell the server to advance the turn by one"""
-    try:
-        messages = get_url(CHECK_MESSAGES_PATH)
-        if len(messages) > 0:
-            error = "Received %i messages from the server for this turn that are not approved.\n" % len(messages)
-            error += "Approve them before advancing to the next turn."
-            await ctx.send(error)
-            return
-    except Exception as e:
-        await ctx.send("There was an error checking the messages:%s" % str(e)[:1000])
-        raise
-    try:
-        res = patch_url(NEXT_TURN_PATH)
-        await ctx.send("Next turn started. This is turn {}, time is now {}".format(res["turn"], res["current_time"]))
-    except Exception as e:
-        await ctx.send("There was an error advancing the turn:%s" % str(e)[:1000])
-        raise
-    try:
-        messages = get_url(GET_MESSAGES_PATH)
-        await ctx.send("Received %i messages from the server" % len(messages))
-    except Exception as e:
-        await ctx.send("There was an error receiving messages:%s" % str(e)[:1000])
-        raise
-    try:
-        for message in messages:
-            await deliver(message)
-    except Exception as e:
-        await ctx.send("There was an error delivering messages:%s" % str(e)[:1000])
-        raise
+       print(red)
+       print(blue)
+       data = {
+           "name_channels": list(blue.keys()) + list(red.keys()),
+           "name_game": name
+       }
+       try:
+           res = post_url(NEW_GAME_PATH, data)
+           message = "Game created\n" \
+                     "Name: %s\n" \
+                     "Time is now %s\n" \
+                     "%i Blue channels\n" \
+                     "%i Red channels" % (res["name"], res["start_time"], len(blue), len(red))
+           await ctx.send(message)
+       except Exception as e:
+           await ctx.send("There was an error creating the game:%s" % str(e)[:1000])
+           raise
 
-
-@bot.command()
-async def start_game(ctx, name):
-    """start a new game"""
-    blue = {}
-    red = {}
-    for entry in ctx.guild.channels:
-        if entry.type == ChannelType.category:
-            if entry.name == RED_CATEGORY:
-                for channel in entry.text_channels:
-                    red[channel.name] = channel.id
-            if entry.name == BLUE_CATEGORY:
-                for channel in entry.text_channels:
-                    blue[channel.name] = channel.id
-
-    print(red)
-    print(blue)
-    data = {
-        "name_channels": list(blue.keys()) + list(red.keys()),
-        "name_game": name
-    }
-    try:
-        res = post_url(NEW_GAME_PATH, data)
-        message = "Game created\n" \
-                  "Name: %s\n" \
-                  "Time is now %s\n" \
-                  "%i Blue channels\n" \
-                  "%i Red channels" % (res["name"], res["start_time"], len(blue), len(red))
-        await ctx.send(message)
-    except Exception as e:
-        await ctx.send("There was an error creating the game:%s" % str(e)[:1000])
-        raise
-
-
-@bot.command()
-async def dummy_start_game(ctx, game_name: str):
-    data = {"name_channels": ["channel1", "channel2", "channel3"],
-            "name_game": game_name}
-    res = post_url("new_game/", data)
-    await ctx.send("A new game " + res["name"] + " has started\n Time is now " + res['start_time'])
-
+    @bot.command()
+    async def next_turn(ctx):
+        """Tell the server to advance the turn by one"""
+        try:
+            messages = get_url(CHECK_MESSAGES_PATH)
+            if len(messages) > 0:
+                error = "Received %i messages from the server for this turn that are not approved.\n" % len(messages)
+                error += "Approve them before advancing to the next turn."
+                await ctx.send(error)
+                return
+        except Exception as e:
+            await ctx.send("There was an error checking the messages:%s" % str(e)[:1000])
+            raise
+        try:
+            res = patch_url(NEXT_TURN_PATH)
+            await ctx.send("Next turn started. This is turn {}, time is now {}".format(res["turn"], res["current_time"]))
+        except Exception as e:
+            await ctx.send("There was an error advancing the turn:%s" % str(e)[:1000])
+            raise
+        try:
+            messages = get_url(GET_MESSAGES_PATH)
+            await ctx.send("Received %i messages from the server" % len(messages))
+        except Exception as e:
+            await ctx.send("There was an error receiving messages:%s" % str(e)[:1000])
+            raise
+        try:
+            for message in messages:
+                await deliver(message)
+        except Exception as e:
+            await ctx.send("There was an error delivering messages:%s" % str(e)[:1000])
+            raise
 
 bot.run(TOKEN)
